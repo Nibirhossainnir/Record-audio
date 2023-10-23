ef99789822cbb206b57dceba1ebab7366f03ee5a
Nibirhossainnir <144318970+Nibirhossainnir@users.noreply.github.com>
2023-10-23 13:00:26 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+var device = navigator.mediaDevices.getUserMedia({audio: true});
+var items = [];
+device.then(stream =>{
+    var recorder = new MediaRecorder(stream);
+    recorder.ondataavailable = e=>{
+        items.push(e.data)
+        if(recorder.state == 'inactive'){
+            var blob = new Blob(items, {type:'audio/webm'});
+            var audio = document.getElementById('audio');
+            var mainaudio = document.createElement('audio');
+            mainaudio.setAttribute('controls','controls');
+            audio.appendChild(mainaudio);
+            mainaudio.innerHTML = '<source src ="'+URL.createObjectURL(blob)+'"type="video/webm"/>';
+        }
+    }
+    recorder.start(100);
+    setTimeout(()=>{
+        recorder.stop();
+    },5000);
+})
